f11bd36b56ca7418531cf482f22ac02754634b9c
Timon Jurschitsch <timon.jurschitsch@proton.me>
2022-09-13 16:02:45 +0200

    Add functionality
    User can generate memes and also download them

--- src/App.js
+++ src/App.js
@@ -1,21 +1,111 @@
 import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useState } from 'react';
 
 function App() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [memeType, setMemeType] = useState('');
+  const [image, setImage] = useState(
+    ' https://api.memegen.link/images/xy/show_us_all_the_memes!.jpg?watermark=memecomplete.com&token=yds9lf2jrx72zoi11jnh ',
+  );
+  let count = 1;
+  const fileName = `/meme_${count}.jpeg`;
+
+  const input = [memeType, topText, bottomText];
+  const corrected = input.map((text) => {
+    return text
+      .replace(' ', '_')
+      .replace('#', '~h')
+      .replace('?', '~q')
+      .replace('/', '~s');
+  });
+  const url = `https://api.memegen.link/images/${corrected[0]}/${corrected[1]}/${corrected[2]}.jpeg`;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setImage(url);
+  };
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 13) {
+      handleSubmit();
+    }
+  };
+  const handleDownload = (event) => {
+    event.preventDefault();
+    fetch(url, {
+      method: 'get',
+      headers: {
+        'Content-type': 'image/jpeg',
+      },
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        // Create blob link to download
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', fileName);
+
+        // Append to html link element page
+        document.body.appendChild(link);
+
+        // Start download
+        link.click();
+
+        // Clean up and remove the link
+        link.parentNode.removeChild(link);
+      });
+
+    count++;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div>
+        {' '}
+        <h1>Create a Meme by yourself!</h1>
+        <img src={image} alt="Created meme" data-test-id="meme-image" />
+      </div>
+      <form>
+        <label htmlFor="top">Top Text</label>
+        <input
+          name="top"
+          id="top"
+          value={topText}
+          onClick={() => setTopText('')}
+          onChange={(event) => setTopText(event.currentTarget.value)}
+        />
+        <br />
+        <label htmlFor="bottom">Bottom Text</label>
+        <input
+          name="bottom"
+          id="bottom"
+          value={bottomText}
+          onClick={() => setBottomText('')}
+          onChange={(event) => setBottomText(event.currentTarget.value)}
+        />
+        <br />
+        <label htmlFor="type">Meme template</label>
+        <input
+          name="type"
+          id="type"
+          value={memeType}
+          onClick={() => setMemeType('')}
+          onChange={(event) => setMemeType(event.currentTarget.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <br />
+        <button onClick={handleSubmit} data-test-id="generate-meme">
+          Generate
+        </button>
+
+        <br />
+        <a href={url} onClick={handleDownload} download>
+          Download
         </a>
-      </header>
+      </form>
     </div>
   );
 }
