d7dfd3db179841c9e18139c7d703b2542e5ee4a3
Timon Jurschitsch <timon.jurschitsch@proton.me>
2022-09-14 14:48:20 +0200

    Add functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const [memeType, setMemeType] = useState('');
-  /*   const [image, setImage] = useState(
+  const [topText, setTopText] = useState('hello');
+  const [bottomText, setBottomText] = useState('there');
+  const [memeType, setMemeType] = useState('bender');
+  const [image, setImage] = useState(
     ' https://api.memegen.link/images/bender/hello/there.jpeg ',
-  ); */
+  );
   let count = 1;
   const fileName = `/meme_${count}.jpeg`;
 
@@ -22,16 +22,9 @@ function App() {
             .replace('/', '~s')
       : text;
   });
-  const url =
-    topText && bottomText && memeType
-      ? `https://api.memegen.link/images${corrected[0]}${corrected[1]}${corrected[2]}.jpg`
-      : 'https://api.memegen.link/images/bender/hello/there.jpeg';
+  const url = `https://api.memegen.link/images${corrected[0]}${corrected[1]}${corrected[2]}.jpg`;
 
-<<<<<<< HEAD
-  /*   const handleSubmit = (event) => {
-=======
-/*   const handleSubmit = (event) => {
->>>>>>> a40e08f9610b9de3eb70b8e275f155fdb8a7758f
+  const handleSubmit = (event) => {
     event.preventDefault();
     setImage(url);
   };
@@ -39,7 +32,7 @@ function App() {
     if (event.keyCode === 13) {
       handleSubmit();
     }
-  }; */
+  };
   const handleDownload = (event) => {
     event.preventDefault();
     fetch(url, {
@@ -74,7 +67,7 @@ function App() {
     <div className="App">
       <div>
         <h1>Create a Meme by yourself!</h1>
-        <img src={url} alt="Created meme" data-test-id="meme-image" />
+        <img src={image} alt="Created meme" data-test-id="meme-image" />
       </div>
       <form>
         <label htmlFor="top">Top text</label>
@@ -84,7 +77,7 @@ function App() {
           value={topText}
           onClick={() => setTopText('')}
           onChange={(event) => setTopText(event.currentTarget.value)}
-          // onKeyPress={handleKeyPress}
+          onKeyPress={handleKeyPress}
         />
         <br />
         <label htmlFor="bottom">Bottom text</label>
@@ -94,7 +87,7 @@ function App() {
           value={bottomText}
           onClick={() => setBottomText('')}
           onChange={(event) => setBottomText(event.currentTarget.value)}
-          // onKeyPress={handleKeyPress}
+          onKeyPress={handleKeyPress}
         />
         <br />
         <label htmlFor="type">Meme template</label>
@@ -104,22 +97,17 @@ function App() {
           value={memeType}
           onClick={() => setMemeType('')}
           onChange={(event) => setMemeType(event.currentTarget.value)}
-          // onKeyPress={handleKeyPress}
+          onKeyPress={handleKeyPress}
         />
         <br />
         <br />
-
-<<<<<<< HEAD
-        <button data-test-id="generate-meme">Generate</button>
-=======
-        <button data-test-id="generate-meme">
+        <button onClick={handleSubmit} data-test-id="generate-meme">
           Generate
         </button>
->>>>>>> a40e08f9610b9de3eb70b8e275f155fdb8a7758f
 
         <br />
         <br />
-        <a href={url} download>
+        <a href={url} onClick={handleDownload} download>
           <button>Download</button>
         </a>
       </form>
